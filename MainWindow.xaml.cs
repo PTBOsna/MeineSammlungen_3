f66c1e41cad09cf3a2055d89dff1c7935f08eae9
Ptbberlin <post@my-jambo.de>
2020-01-30 13:36:49 +0100

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MeineSammlungen_3
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //DataClassesSammlungenDataContext con = new DataClassesSammlungenDataContext();
+            List<Module> modules = (from m in Admin. con.Module select m).ToList();
+            ModulGrid. ItemsSource = modules;
+
+        }
+
+        private void ModulGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Module selected = ModulGrid.SelectedItem as Module; if (selected == null)
+            {
+                return;
+            }
+            var myDetail = from d in Admin.con.Grunddaten where d.Modul == selected.ID select d;
+            GdGrid.ItemsSource = myDetail.ToList();
+
+
+        }
+    }
+}
---
+++ ModulGrunddaten.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeineSammlungen_3
+{
+    class Admin
+    {
+        public static DataClassesSammlungenDataContext con = new DataClassesSammlungenDataContext();
+        #region Grunddaten
+        public static void AddGrunddaten(Grunddaten _ModulMikro)
+        {
+            using (DataClassesSammlungenDataContext conn = new DataClassesSammlungenDataContext())
+            {
+                conn.Grunddaten.InsertOnSubmit(_ModulMikro);
+                conn.SubmitChanges();
+            }
+
+        }
+
+        public static void EditGrunddaten(Grunddaten _ModulMikro)
+        {
+            using (DataClassesSammlungenDataContext conn = new DataClassesSammlungenDataContext())
+            {
+                Grunddaten gd = (from g in conn.Grunddaten where g.ID == _ModulMikro.ID select g).FirstOrDefault();
+                gd.LfdNr = _ModulMikro.LfdNr;
+                gd.Modul = _ModulMikro.Modul;
+                gd.Nr = _ModulMikro.Nr;
+                gd.Objekt = _ModulMikro.Objekt;
+                gd.Detail = _ModulMikro.Detail;
+                gd.Bemerkung = _ModulMikro.Bemerkung;
+                gd.Ablageort = _ModulMikro.Ablageort;
+                gd.Ablageort_neu = _ModulMikro.Ablageort_neu;
+                gd.Erstellt = _ModulMikro.Erstellt;
+                gd.Geaendert = _ModulMikro.Geaendert;
+                gd.Checked = _ModulMikro.Checked;
+                gd.ImgCount = _ModulMikro.ImgCount;
+                conn.SubmitChanges();
+            }
+
+        }
+        public static void DeleteGrunddat(Grunddaten _ModulMikro)
+        {
+            using (DataClassesSammlungenDataContext conn = new DataClassesSammlungenDataContext())
+            {
+                var gd = from g in conn.Grunddaten where g.ID == _ModulMikro.ID select g;
+                conn.Grunddaten.DeleteAllOnSubmit(gd);
+                conn.SubmitChanges();
+            }
+
+        }
+        #endregion
+        #region Modul
+        public static void AddModule(Module _ModulMikro)
+        {
+            using (DataClassesSammlungenDataContext conn = new DataClassesSammlungenDataContext())
+            {
+                conn.Module.InsertOnSubmit(_ModulMikro);
+                conn.SubmitChanges();
+            }
+
+        }
+
+        public static void EditModule(Module _ModulMikro)
+        {
+            using (DataClassesSammlungenDataContext conn = new DataClassesSammlungenDataContext())
+            {
+                Module md = (from m in conn.Module where m.ID == _ModulMikro.ID select m).FirstOrDefault();
+                md.Modul = _ModulMikro.Modul;
+                conn.SubmitChanges();
+            }
+
+        }
+        public static void DeleteModule(Module _ModulMikro)
+        {
+            using (DataClassesSammlungenDataContext conn = new DataClassesSammlungenDataContext())
+            {
+                var gd = from g in conn.Module where g.ID == _ModulMikro.ID select g;
+                conn.Module.DeleteAllOnSubmit(gd);
+                conn.SubmitChanges();
+            }
+
+        }
+        #endregion
+        #region MikroMakro
+        public static void AddModulMikro(ModulMikro _ModulMikro)
+        {
+            using (DataClassesSammlungenDataContext conn = new DataClassesSammlungenDataContext())
+            {
+                conn.ModulMikro.InsertOnSubmit(_ModulMikro);
+                conn.SubmitChanges();
+            }
+
+        }
+
+        public static void EditModulMikro(ModulMikro _ModulMikro)
+        {
+            using (DataClassesSammlungenDataContext conn = new DataClassesSammlungenDataContext())
+            {
+                ModulMikro md = (from m in conn.ModulMikro where m.ID == _ModulMikro.ID select m).FirstOrDefault();
+                md.Aufhellung = _ModulMikro.Aufhellung;
+                md.Einschluss = _ModulMikro.Einschluss;
+                md.Farbung = _ModulMikro.Farbung;
+                md.Fixierung = _ModulMikro.Fixierung;
+                md.Grunddaten_ID = _ModulMikro.Grunddaten_ID;
+                md.Hineise = _ModulMikro.Hineise;
+                md.Schnittart = _ModulMikro.Schnittart;
+                md.Schnittebene = _ModulMikro.Schnittebene;
+                conn.SubmitChanges();
+            }
+
+        }
+        public static void DeleteModulMikro(ModulMikro _ModulMikro)
+        {
+            using (DataClassesSammlungenDataContext conn = new DataClassesSammlungenDataContext())
+            {
+                var gd = from g in conn.ModulMikro where g.ID == _ModulMikro.ID select g;
+                conn.ModulMikro.DeleteAllOnSubmit(gd);
+                conn.SubmitChanges();
+            }
+
+        }
+        #endregion
+    }
+
+}
